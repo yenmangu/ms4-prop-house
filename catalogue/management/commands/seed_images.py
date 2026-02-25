af09dc7ab445502f221b4fcc1feac1ded6bb2e74
Robert John Shelford <robert.shelford@googlemail.com>
2026-02-25 16:49:26 +0000

    feat(seed_images): add handshake and response to cloudinary seed script

--- catalogue/management/commands/seed_images.py
+++ catalogue/management/commands/seed_images.py
@@ -11,6 +11,10 @@ import cloudinary.uploader
 # write one process and tell Python to repeat
 
 
+# Phase 3-4 - Handshake/Preparation - tell Cloudinary where the image is located
+# Pass this path to the uploader
+# Take response with new URL and set the product image to the new URL
+## Wrap all this in try..except block to ensure graceful failure.
 class Command(BaseCommand):
     help = "A short description for the --help flag"
 
@@ -22,6 +26,38 @@ class Command(BaseCommand):
 
         # The loop
         for product in products:
+            # Check product actually has image
+
+            if not product.image:
+                self.stdout.write(
+                    self.style.WARNING(f"Skipping {product.name}: No image found")
+                )
+                continue
+
+            try:
+                # 1. Upload
+                upload_data = cloudinary.uploader.upload(
+                    product.image.path,
+                    folder="catalogue_seeds",
+                )
+
+                # 2. Capture
+                cloudinary_url = upload_data.get("secure_url")
+
+                # 3. Update
+                # Change product image field to new url and save
+
+                product.image = cloudinary_url
+                product.save()
+
+                self.stdout.write(
+                    self.style.SUCCESS(f"Successfully uploaded {product.name}")
+                )
+            except Exception as e:
+                self.stdout.write(
+                    self.style.ERROR(f"Failed to upload {product.name}: {e}")
+                )
+
             self.stdout.write(f"Currently processing:  {product.name}")
 
-        pass
+        self.stdout.write(self.style.SUCCESS("Seeding complete!"))
