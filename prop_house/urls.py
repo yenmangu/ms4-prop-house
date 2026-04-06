f4493f7640cae7bf1ce5137fa89d6dd197d576ad
Robert John Shelford <robert.shelford@googlemail.com>
2026-04-06 23:37:58 +0100

    refactor(prop_house.urls): ensure urls patterns resolved in correct order

--- prop_house/urls.py
+++ prop_house/urls.py
@@ -17,22 +17,23 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path(
-        "",
-        include(
-            "catalogue.urls",
-            namespace="catalogue",
-        ),
-    ),
-    path(
         "basket/",
         include(
             "basket.urls",
             namespace="basket",
+        ),
+    ),
+    path(
+        "",
+        include(
+            "catalogue.urls",
+            namespace="catalogue",
         ),
     ),
 ]
