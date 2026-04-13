27f92aa7fd3b80bd13f9591562ca6103582eb54e
Robert John Shelford <robert.shelford@googlemail.com>
2026-04-14 00:47:17 +0100

    fix(basket): remove redundantquantity recalculation which breaks F logic

--- basket/models.py
+++ basket/models.py
@@ -159,16 +159,12 @@ class Basket(models.Model):
             # Use F here to remove any race condition
             # i.e.: user adds to basket twice rapidly
             line.quantity = models.F("quantity") + int(quantity)
-        line.quantity = (
-            (line.quantity + int(quantity)) if not created else int(quantity)
-        )
 
         line.save()
 
         # Because `F() `is used, Line object doesnt have new value yet. # Must manually call `refresh_from_db()`:
         line.refresh_from_db()
         self.save()
-
         return line
 
     def _remove(self, product_id):
