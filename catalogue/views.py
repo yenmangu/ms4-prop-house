ea0f9a0bc5006c8704bbcd4cf8fed797fd421cd5
Robert John Shelford <robert.shelford@googlemail.com>
2026-04-20 22:05:40 +0100

    feat(csrf): add method to class based view to ensure csrf cookie set at all time

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views import generic
 from view_breadcrumbs import ListBreadcrumbMixin, DetailBreadcrumbMixin
 from .models import Product
@@ -6,6 +8,7 @@ from .filters import ProductFilter
 
 
 # Create your views here.
+@method_decorator(ensure_csrf_cookie, name="dispatch")
 class ProductListView(
     ListBreadcrumbMixin,
     generic.ListView,
