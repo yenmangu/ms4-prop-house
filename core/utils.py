4200dea769e7596850b6f05fb249509c4e6555dd
Robert John Shelford <robert.shelford@googlemail.com>
2026-05-15 15:07:02 +0100

    feat(core): add keyword args tohget_admin_link for granular attribute control

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,21 @@
 from django.utils.html import format_html
 
 
-def get_admin_link(url: str, label: str) -> str:
+def get_admin_link(
+    url: str,
+    label: str,
+    hover_text=None,
+) -> str:
     """
     Returns a safe HTML anchor tag for the Django admin.
+    Supports optional full title hover-tooltip.
     """
-    return format_html('<a href="{}">{}</a>', url, label)
+    if not url:
+        return label
+
+    return format_html(
+        '<a href="{url}" title="{title}">{label}</a>',
+        url=url,
+        title=hover_text or label,
+        label=label,
+    )
--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,9 +1,19 @@
+from typing import TYPE_CHECKING
+
 from django.contrib import admin
 from django.db.models import QuerySet
 from django.http import HttpRequest
+from django.template.defaultfilters import truncatechars
+from django.urls import reverse
 from django.utils import timezone
 from .models import HireRecord, StockItem
 from .services import dispatch_hire_records
+from core.utils import get_admin_link
+
+if TYPE_CHECKING:
+    from commerce.models import Order
+
+    obj: HireRecord
 
 
 @admin.register(StockItem)
@@ -74,9 +84,10 @@ def mark_as_dispatched(
 class HireRecordAdmin(admin.ModelAdmin):
     list_display = (
         "id",
-        "get_order_id",
-        "order_item",
-        "stock_item",
+        "link_order",
+        "link_user",
+        "link_order_item",
+        "link_stock_item",
         "out_date",
         "due_date",
     )
@@ -84,6 +95,7 @@ class HireRecordAdmin(admin.ModelAdmin):
     list_filter = (
         "out_date",
         "due_date",
+        "order_item__order__user__email",
     )
 
     readonly_fields = ("order_item",)
@@ -92,5 +104,110 @@ class HireRecordAdmin(admin.ModelAdmin):
         "order_item__order",
         "stock_item",
     )
+
+    @admin.display(
+        description="Customer", ordering="order_item__order__user"
+    )
+    def get_customer(self, obj: "HireRecord"):
+        """
+        Get User by traversing relationship:
+        HireRecord -> OrderItem -> Order -> User
+        """
+
+        try:
+            order = obj.order_item.order
+            if order.user:
+                return order.user.email
+            return f"{order.email} (Guest)"
+        except AttributeError:
+            return "No User"
+
+    @admin.display(
+        description="Customer Email",
+        ordering="order_item__order__user__email",
+    )
+    def link_user(self, obj: "HireRecord"):
+        """
+        Create link between HireRecord and related User via the Order
+        """
+        try:
+            user = obj.order_item.order.user
+            if user:
+                email = user.email
+                url = reverse(
+                    "admin:accounts_user_change", args=[user.pk]
+                )
+                return get_admin_link(
+                    url=url,
+                    label=str(email),
+                )
+        except AttributeError:
+            pass
+
+        return "Guest/No User"
+
+    @admin.display(
+        description="Order ID", ordering="order_item__order__id"
+    )
+    def link_order(self, obj: "HireRecord"):
+        """
+        Create link between HireRecord and related Order
+        """
+        if obj.order_item and obj.order_item.order:
+            order_id = obj.order_item.order.pk
+            url = reverse(
+                "admin:commerce_order_change", args=[order_id]
+            )
+            return get_admin_link(
+                url=url,
+                label=f"Order#{order_id}",
+            )
+        return "No Order"
+
+    @admin.display(
+        description="Order Item", ordering="order_item__pk"
+    )
+    def link_order_item(self, obj: "HireRecord"):
+        """
+        Create link between HireRecord and associated OrderItem.
+        """
+
+        try:
+            order_item = obj.order_item
+            if order_item:
+                id = order_item.pk
+                url = reverse(
+                    "admin:commerce_orderitem_change", args=[id]
+                )
+                return get_admin_link(
+                    url=url,
+                    label=f"Order Item ID: {id}",
+                )
+        except AttributeError:
+            pass
+        return "No associated Order Item"
+
+    @admin.display(
+        description="Stock Item", ordering="stock_item__serial_number"
+    )
+    def link_stock_item(self, obj: "HireRecord"):
+        """
+        Create link between HireRecord and related StockItem.
+        Truncates the returned label to keep dasboard clean.
+        """
+
+        TRUNC_VAL = 10
+
+        if obj.stock_item:
+            url = reverse(
+                "admin:warehouse_stockitem_change",
+                args=[obj.stock_item.pk],
+            )
+            return get_admin_link(
+                url=url,
+                label=truncatechars(str(obj.stock_item), TRUNC_VAL),
+                hover_text=str(obj.stock_item),
+            )
+        return "Unassigned"
 
     actions = [mark_as_dispatched]
